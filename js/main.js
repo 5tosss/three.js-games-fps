c3b6f16c4d233779ba83ff126b4c55e383867677
Diego Quintos <ravenygo27.0@gmail.com>
2025-04-30 01:49:01 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 import * as THREE from "three";
 
-import Stats from "./three/examples/jsm/libs/stats.module.js";
+import Stats from ",./three/examples/jsm/libs/stats.module.js";
 
-import { GLTFLoader } from "./three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "../three/examples/jsm/loaders/GLTFLoader.js";
 
-import { Octree } from "./three/examples/jsm/math/Octree.js";
-import { OctreeHelper } from "./three/examples/jsm/helpers/OctreeHelper.js";
+import { Octree } from "../three/examples/jsm/math/Octree.js";
+import { OctreeHelper } from "../three/examples/jsm/helpers/OctreeHelper.js";
 
-import { Capsule } from "./three/examples/jsm/math/Capsule.js";
+import { Capsule } from "../three/examples/jsm/math/Capsule.js";
 
-import { GUI } from "./three/examples/jsm/libs/lil-gui.module.min.js";
+import { GUI } from "../three/examples/jsm/libs/lil-gui.module.min.js";
 
 const clock = new THREE.Clock();
 
