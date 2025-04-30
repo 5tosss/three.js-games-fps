06507451d7fe67acb7e2b6ab373eb9045bd4faf4
Diego Quintos <ravenygo27.0@gmail.com>
2025-04-30 01:49:16 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-import Stats from ",./three/examples/jsm/libs/stats.module.js";
+import Stats from "../three/examples/jsm/libs/stats.module.js";
 
 import { GLTFLoader } from "../three/examples/jsm/loaders/GLTFLoader.js";
 
